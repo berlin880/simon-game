a8207554d54f0c4801fcb8249c994128e63d2a22
berlin880 <=abdelrhmanadel517@gmail.com>
2026-06-24 20:51:35 +0300

    Ready for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    buildActivity: false,
-  },
-  experimental: {
-    appDir: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
-  },  
-  /* config options here */
+  },
 };
 
 export default nextConfig;
